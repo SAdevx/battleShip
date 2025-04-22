19890afd440924b5161e181c0a3c5b33641b6183
stephenwiafe <sadams.dev@gmail.com>
2025-04-22 01:41:15 -0400

    Create ship boards

--- src/battleship.js
+++ src/battleship.js
@@ -9,18 +9,39 @@ function getBoard(){
     board2.classList.add("board");
     
     for(let i = 1; i <= 100; i++){
-        const player1Cells = document.createElement("div");
-        const player2Cells = document.createElement("div");
-
-        player1Cells.classList.add("cells");
-        player2Cells.classList.add("cells");
-
-        board1.appendChild(player1Cells);
-        board2.appendChild(player2Cells);
+       
     }
-    body.appendChild(board1);
-    body.appendChild(board2);
+
+    for(let i = 0; i < 10; i++) {
+        for(let j = 0; j < 10; j++) {
+            const player1Cells = document.createElement("div");
+            const player2Cells = document.createElement("div");
+    
+            player1Cells.classList.add("cells", "player-cell");
+            player2Cells.classList.add("cells", "computer-cell");
+
+            player1Cells.dataset.cell = `${i}${j}`;
+            player2Cells.dataset.cell = `${i}${j}`;
+    
+            board1.appendChild(player1Cells);
+            board2.appendChild(player2Cells);
+            
+        }
+        body.appendChild(board1);
+        body.appendChild(board2);
+    }
 }
 
+function placeShipDOM(startCoord, endCoord){
+    for (let i = startCoord[0]; i <= endCoord[0]; i++) {
+       for (let j = startCoord[1]; j <= endCoord[1]; j++) {
+          const cells = document.querySelectorAll(`[data-cell="${i}${j}"]`);
+          cells.forEach(cell => {
+            cell.style.backgroundColor = "violet";
+          });
+       }
+    }
+ }
 
-export { getBoard };
+
+export { getBoard, placeShipDOM };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,67 @@
 import { Ship } from "./ship";
 import { GameBoard } from "./gameboard";
 import { Player } from "./player";
-import { getBoard } from "./battleship";
+import { getBoard, placeShipDOM } from "./battleship";
 
 getBoard();
+
+const playerBoard = document.querySelector(".player-cell");
+const computerBoard = document.querySelectorAll(".computer-cell");
+
+
+const p1GameBoard = new GameBoard();
+const p2GameBoard = new GameBoard();
+
+p1GameBoard.createBoard();
+p2GameBoard.createBoard();
+
+//player board
+p1GameBoard.placeShip([0,0],[0,0], new Ship(1));
+p1GameBoard.placeShip([4,8],[4,8], new Ship(1));
+p1GameBoard.placeShip([9,5],[9,5], new Ship(1));
+p1GameBoard.placeShip([3,3],[3,3], new Ship(1));
+p1GameBoard.placeShip([6,2],[7,2], new Ship(2));
+p1GameBoard.placeShip([6,4],[7,4], new Ship(2));
+p1GameBoard.placeShip([9,8],[9,9], new Ship(2));
+p1GameBoard.placeShip([1,4],[1,6], new Ship(3));
+p1GameBoard.placeShip([7,7],[7,9], new Ship(3));
+p1GameBoard.placeShip([6,0],[9,0], new Ship(4));
+
+//computer board
+p2GameBoard.placeShip([0,0],[0,0], new Ship(1));
+p2GameBoard.placeShip([4,8],[4,8], new Ship(1));
+p2GameBoard.placeShip([9,5],[9,5], new Ship(1));
+p2GameBoard.placeShip([3,3],[3,3], new Ship(1));
+p2GameBoard.placeShip([6,2],[7,2], new Ship(2));
+p2GameBoard.placeShip([6,4],[7,4], new Ship(2));
+p2GameBoard.placeShip([9,8],[9,9], new Ship(2));
+p2GameBoard.placeShip([1,4],[1,6], new Ship(3));
+p2GameBoard.placeShip([7,7],[7,9], new Ship(3));
+p2GameBoard.placeShip([6,0],[9,0], new Ship(4));
+
+//show ship on UI
+placeShipDOM([0,0],[0,0]);
+placeShipDOM([4,8],[4,8]);
+placeShipDOM([9,5],[9,5]);
+placeShipDOM([3,3],[3,3]);
+placeShipDOM([6,0],[9,0]);
+placeShipDOM([6,2],[7,2]);
+placeShipDOM([6,4],[7,4]);
+placeShipDOM([1,4],[1,6]);
+placeShipDOM([7,7],[7,9]);
+placeShipDOM([9,8],[9,9]);
+
+
+const player1 = new Player("Player", p1GameBoard);
+const player2 = new Player("Computer", p2GameBoard);
+
+//first have a function to place the ships on both boards manually first
+
+computerBoard.forEach((cell) => {
+   cell.addEventListener("click", (e)=>{
+   console.log(e.target.dataset.cell);
+   });
+});
+
+
+
