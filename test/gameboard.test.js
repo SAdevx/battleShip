59cbedd5e14fb03d4e29901e47af9885eedeebd2
stephenwiafe <sadams.dev@gmail.com>
2025-05-16 00:01:50 -0400

    Fix battlehip test

--- test/gameboard.test.js
+++ test/gameboard.test.js
@@ -1,51 +1,48 @@
-import { GameBoard } from "../src/gameboard";
-import { Ship } from "../src/ship";
+import { GameBoard } from '../src/gameboard'
+import { Ship } from '../src/ship'
 
 describe('test gameboard class', () => {
-    const board = new GameBoard();
-    board.createBoard();
-    board.placeShip([5,5],[6,5], new Ship(2));
-    
-    test('test receiveAttack method', () => { 
-        expect(board.receiveAttack([5,5])).toBe(true);
-    });
+  const board = new GameBoard();
+  board.createBoard();
+  board.placeShip([5, 5], [6, 5], new Ship(2))
 
-    test('test receiveAttack method', () => { 
-        expect(board.receiveAttack([1,1])).toBe(false);
-    });
-});
+  test('test receiveAttack method', () => {
+    expect(board.receiveAttack([5, 5])).toBe(1);
+  })
+
+  test('test receiveAttack method', () => {
+    expect(board.receiveAttack([1, 1])).toBe(2);
+  })
+})
 
 describe('test gameboard class', () => {
-    const board = new GameBoard();
-    board.createBoard();
-    const ship1 = new Ship(1);
-    const ship2 = new Ship(3);
-    const ship3 = new Ship(3);
+  const board = new GameBoard();
+  board.createBoard();
+  const ship1 = new Ship(1);
+  const ship2 = new Ship(3);
+  const ship3 = new Ship(3);
 
-    board.placeShip([5,5],[5,5], ship1);
-    board.placeShip([6,3],[8,3], ship2);
-    board.placeShip([4,5],[6,4], ship3);
+  board.placeShip([5, 5], [5, 5], ship1);
+  board.placeShip([6, 3], [8, 3], ship2);
+  board.placeShip([7, 7], [7, 9], ship3);
 
-    ship1.hit();
-    ship2.hit();
-    ship2.hit();
-    ship2.hit();
+  ship1.hit();
+  ship2.hit();
+  ship2.hit();
+  ship2.hit();
+  ship3.hit();
+  ship3.hit();
+
+  test('test allShipSunk method', () => {
+    expect(board.allShipSunk([[5,5], [6,3]])).toBe(true);
+  })
+
+  test('test allShipSunk method', () => {
+    expect(board.allShipSunk([[5,5], [6,3], [7,7]])).toBe(false);
+  })
+
+  test('test allShipSunk method', () => {
     ship3.hit();
-    ship3.hit();
-    
-    test('test allShipSunk method', () => { 
-        expect(board.allShipSunk([ship1,ship2])).toBe(true);
-    });
-
-    test('test allShipSunk method', () => { 
-        expect(board.allShipSunk([ship1,ship2,ship3])).toBe(false);
-    });
-
-    test('test cellTaken method', () => { 
-        expect(board.cellTaken([5,5],[6,5])).toBe(true);
-    });
-
-    test('test cellTaken method', () => { 
-        expect(board.cellTaken([1,1],[3,1])).toBe(false);
-    });
-});
+    expect(board.allShipSunk([[5,5], [6,3], [7,7]])).toBe(true);
+  })
+})
--- test/ship.test.js
+++ test/ship.test.js
@@ -1,19 +1,19 @@
-import { Ship } from "../src/ship";
+import { Ship } from '../src/ship'
 
 describe('test ship class', () => {
-    test('test hit function when false', () => {
-        const ship = new Ship(4);
-        ship.hit();
-        ship.hit();
-        expect(ship.isSunked()).toBeFalsy();
-    });
+  test('test hit function when false', () => {
+    const ship = new Ship(4)
+    ship.hit()
+    ship.hit()
+    expect(ship.isSunk()).toBeFalsy()
+  })
 
-    test('test hit function when true', () => {
-        const ship = new Ship(4);
-        ship.hit();
-        ship.hit();
-        ship.hit();
-        ship.hit();
-        expect(ship.isSunked()).toBeTruthy();
-    });
-});
+  test('test hit function when true', () => {
+    const ship = new Ship(4)
+    ship.hit()
+    ship.hit()
+    ship.hit()
+    ship.hit()
+    expect(ship.isSunk()).toBeTruthy()
+  })
+})
